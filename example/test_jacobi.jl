28057057dc0a5c9d89b04e960ce531f5f1d04ff7
Marcus Webb <marcusdavidwebb@gmail.com>
2025-03-17 10:41:40 +0000

    Update example file

--- example/test_jacobi.jl
+++ example/test_jacobi.jl
@@ -6,39 +6,30 @@ A = randn(n,n);
 A = A' * A;
 
 A1 = copy(A);
-Λⱼ, Vⱼ, Para = Jacobi(A1)
+Λᵣ, Vᵣ = eigen!(A1);
 
 A2 = copy(A);
-Λᵣ, Vᵣ = eigen!(A2);
-
-A3 = copy(A);
-Λⱼ, Vⱼ, Para = jacobi_eigen!(A3)
+Λⱼ, Vⱼ, Para = jacobi_eigen!(A2)
 
 println("Forward Error = $(maximum(abs.(sort(Λⱼ) - sort(Λᵣ))./abs.(sort(Λᵣ))))");
 println("Backward Error = $(norm(Vⱼ*diagm(Λⱼ)*Vⱼ'-A)/norm(A))");
 println("Orthogonality Error = $(norm(Vⱼ'*Vⱼ - I))");
 
-# the original code is type stable (i.e. the compiler can work out what type all the variables are):
-@code_warntype Jacobi(A1);
-
 # so is the new version:
-@code_warntype jacobi_eigen!(A3);
+@code_warntype jacobi_eigen!(A2);
 
 # timing and memory allocation comparison:
-# many Gigabytes of memory is created
+
 A1 = copy(A);
-@time Jacobi(A1);
+@time eigen!(A1);
 
 A2 = copy(A);
-@time eigen!(A2);
-
-A3 = copy(A);
 @time jacobi_eigen!(A3);
 
 
 # check the memory allocation of the internal function:
 tol = sqrt(n) * eps()/2
-V = Matrix{Float64}(I,n,n)
+V = Matrix{Float64}(I,n,n);
 J = zeros(2,2)
 tmp1 = zeros(2,n)
 tmp2 = zeros(2,n)
